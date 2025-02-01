ece8cc7d9e88448a94d66e4b9e3feb9c3d03c6d8
alimkanova-t-auca-2022 <alimkanova_t@auca.kg>
2025-02-01 22:33:34 +0600

    edit

--- lab07/mail.py
+++ lab07/mail.py
@@ -7,7 +7,7 @@ from email.mime.multipart import MIMEMultipart
 smtp_server = "smtp.gmail.com"
 smtp_port = 587
 sender_email = "tmrlmknv@gmail.com"  # Replace with your email address
-sender_password = "zfon jsxe wwnx onpv"     # Replace with your email password or app-specific password
+sender_password = "zfon jsxe wwnx TTonpvTT"     # Replace with your email password or app-specific password
 receiver_email = "talimkanova13@gmail.com"  # Replace with the recipient's email
 
 # HTML email content
